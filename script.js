e4eff7aa08990ad03100919ba167839a32a67b0e
Elder Flame Doron <adrianl87@nycstudents.net>
2025-05-16 10:23:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let data;
+
+async function init(){
+  let link = "https://data.cityofnewyork.us/resource/8m42-w767.json";
+  info = await fetch(link);
+  data = await info.json();
+  alert("Data loaded");
+}
+
+function getComplaints(){
+  let borough = document.getElementById("borough").value;
+  for(let i = 0; i < data.length; i++){
+    let complaint = data[i];
+    if(complaint.borough == borough){
+      let front = "";
+      front += `<div class="fitted card" style="height:300px;">`;
+      front += `     <h3>${complaint.complaint_type}</h3>`;
+      front += `     <p>${complaint.borough}</p>`;
+      front += `     <p>${complaint.incident_zip}</p>`;
+      front += `     <p>${complaint.descriptor}</p><hr>`;
+      front += `     <p>${complaint.agency_name}<br>(${complaint.status})</p>`;
+      front += `</div>`;
+
+      let back = `<div class="map" id=${complaint.unique_key} > ${complaint.unique_key}</div>` 
+
+      let flipcard = new FlipCard(front, back);
+      flipcard.render("output");
+      
+    
+      showMap(complaint.latitude,complaint.longitude,complaint.unique_key )
+
+    }
+  }
+}
+
+function showMap(lat,lon,id){
+  let location = [lat,lon]
+  map = L.map(id).setView(location, 14);
+  const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 18,
+    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+  }).addTo(map);
+
+  let marker = L.marker(location).addTo(map);
+} 
