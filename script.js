75b27dd87060f7e2c5549c20253e32cfbae58982
Elder Flame Doron <adrianl87@nycstudents.net>
2025-05-16 10:37:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,32 +7,6 @@ async function init(){
   alert("Data loaded");
 }
 
-function getComplaints(){
-  let borough = document.getElementById("borough").value;
-  for(let i = 0; i < data.length; i++){
-    let complaint = data[i];
-    if(complaint.borough == borough){
-      let front = "";
-      front += `<div class="fitted card" style="height:300px;">`;
-      front += `     <h3>${complaint.complaint_type}</h3>`;
-      front += `     <p>${complaint.borough}</p>`;
-      front += `     <p>${complaint.incident_zip}</p>`;
-      front += `     <p>${complaint.descriptor}</p><hr>`;
-      front += `     <p>${complaint.agency_name}<br>(${complaint.status})</p>`;
-      front += `</div>`;
-
-      let back = `<div class="map" id=${complaint.unique_key} > ${complaint.unique_key}</div>` 
-
-      let flipcard = new FlipCard(front, back);
-      flipcard.render("output");
-      
-    
-      showMap(complaint.latitude,complaint.longitude,complaint.unique_key )
-
-    }
-  }
-}
-
 function showMap(lat,lon,id){
   let location = [lat,lon]
   map = L.map(id).setView(location, 14);
