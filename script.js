4a10109ba2ffdb9b3cbb0a28de844b2bafa53f2d
A-Id1ot <gordont13@nycstudents.net>
2025-05-21 10:30:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,37 @@ function showMap(lat,lon,id){
 
   let marker = L.marker(location).addTo(map);
 } 
+function FireIncident(){
+  let bx = 0, br = 0, t = 0, q = 0, m = 0, si = 0;
+  for( let i = 0; i < data.length; i++ ){
+    if ( data[i].alarm_box_borough == "BRONX" ) {
+      bx++;
+    }else if ( data[i].alarm_box_borough == "BROOKLYN" ) {
+      br++;
+    }else if ( data[i].alarm_box_borough == "QUEENS" ) {
+      q++;
+    }else if ( data[i].alarm_box_borough == "MANHATTAN" ) {
+      m++;
+    }else {
+      si++;
+    }
+  }
+  let chartData = [
+    ["BRONX",bx],
+    ["BROOKLYN",br],
+    ["QUEENS",q],
+    ["MANHATTAN",m],
+	  ["RICHMOND / STATEN ISLAND",si]
+  ];
+  let chartType = document.getElementById("chartType").value;
+  displayChart(chartData,"chart", chartType);
+}
+function displayChart( data, id, type ){
+  let chart = c3.generate({
+    bindto: '#' + id,
+    data: {
+      columns: data,
+      type:type
+    }
+  });
+}
