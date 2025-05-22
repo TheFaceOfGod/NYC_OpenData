e09a82f7446d1399504417d3713b4802c6404921
Elder Flame Doron <adrianl87@nycstudents.net>
2025-05-22 10:49:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,60 @@ async function init(){
   data = await info.json();
   alert("Data loaded");
 }
+///////////////
 
-function showMap(lat,lon,id){
-  let location = [lat,lon]
-  map = L.map(id).setView(location, 14);
+let data;
+let map = undefined;
+
+async function displayLocation(){
+  let lat = document.getElementById("lat").value;
+  let lon = document.getElementById("lon").value;
+  let location = [lat, lon];
+  // if user inputs address
+  let address = document.getElementById("address");  
+  if(address.value != ""){
+    location = await geocodeWithNominatim(address.value);
+  }
+  
+  showMap(location);
+}
+
+
+
+function showMap(location){	
+  if (map) {
+    map.remove();
+  }
+  map = L.map("map").setView(location, 14);
+
   const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 18,
     attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
   }).addTo(map);
 
   let marker = L.marker(location).addTo(map);
-} 
+}     
+
+const geocodeWithNominatim = async (address) => {
+  const encoded = encodeURIComponent(address);
+  const url = `https://nominatim.openstreetmap.org/search?q=${encoded}&format=json`;
+
+  try {
+    const response = await fetch(url);
+    const results = await response.json();
+
+    if (results.length > 0) {
+      const { lat, lon } = results[0];
+      console.log(`Latitude: ${lat}, Longitude: ${lon}`);
+      return [lat,lon];
+    } else {
+      console.log('No results found.');
+    }
+  } catch (err) {
+    console.error('Error:', err);
+  }
+};
+///////////////
 function FireIncident(){
   let bx = 0, br = 0, t = 0, q = 0, m = 0, si = 0;
   for( let i = 0; i < data.length; i++ ){
