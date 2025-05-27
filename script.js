45bbb921dcc667097ba160383175bf76903cacf4
Elder Flame Doron <adrianl87@nycstudents.net>
2025-05-27 10:51:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let data, info, output;
+let data, info, output, build;
 
 async function init(){
   let link = "https://data.cityofnewyork.us/resource/8m42-w767.json?$limit=1000";
@@ -6,6 +6,20 @@ async function init(){
   data = await info.json();
   alert("Data loaded");
 }
+for(let i = 0; i < data.length; i++){
+    let complaint = data[i];
+	let lat = complaint.latitude;
+	let lon = complaint.longitude;
+    build += `<div class="fitted card">`;
+    build += `     <h3>${complaint.complaint_type}</h3>`;    
+	build += `	   <hr>`;
+	build += `     <p>Location: ${complaint.incident_address}</p>`;
+	build += `     <h4>${complaint.borough}</h4>`;
+	if(lat && lon){
+        build += `<input type='button' value='Map' onclick="showMap( ${lat},${lon} )">`;
+    }
+    build += `</div>`;
+  }
 let map = undefined;
 async function displayLocation(){
   let lat = document.getElementById("lat").value;
