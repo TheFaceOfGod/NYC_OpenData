a0d17e7cec97e0dec904dd2a2e427fc654667d10
A-Id1ot <gordont13@nycstudents.net>
2025-05-28 10:36:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ async function init(){
   let link = "https://data.cityofnewyork.us/resource/8m42-w767.json?$limit=1000";
   info = await fetch(link);
   data = await info.json();
-  alert("Data loaded");
 }
 let map = undefined;
 async function displayLocation(){
@@ -31,20 +30,18 @@ function showMap(location){
     let complaint = data[i];
     build += `<div class="fitted card">`;
     build += `     <h3>${complaint.incident_borough}</h3>`;    
-	build += `	   <hr>`;
-	build += `     <p>Location: ${complaint.incident_borough}</p>`;
-	build += `     <h4>${complaint.incident_borough}</h4>`;
+    build += `	   <hr>`;
+    build += `     <p>Location: ${complaint.incident_borough}</p>`;
+    build += `     <h4>${complaint.incident_borough}</h4>`;
     build += `</div>`;
   }
 }     
 const geocodeWithNominatim = async (address) => {
   const encoded = encodeURIComponent(address);
   const url = `https://nominatim.openstreetmap.org/search?q=${encoded}&format=json`;
-
   try {
     const response = await fetch(url);
     const results = await response.json();
-
     if (results.length > 0) {
       const { lat, lon } = results[0];
       console.log(`Latitude: ${lat}, Longitude: ${lon}`);
@@ -58,16 +55,17 @@ const geocodeWithNominatim = async (address) => {
 };
 function FireIncident(){
   let bx = 0, br = 0, t = 0, q = 0, m = 0, si = 0;
+  let alarm = document.getElementById("alarm");
   for( let i = 0; i < data.length; i++ ){
-    if ( data[i].alarm_box_borough == "BRONX" ) {
+    if ( data[i].alarm_box_borough == "BRONX" && data[i].alarm_source_description_tx == alarm) {
       bx++;
-    }else if ( data[i].alarm_box_borough == "BROOKLYN" ) {
+    }else if ( data[i].alarm_box_borough == "BROOKLYN" && data[i].alarm_source_description_tx == alarm) {
       br++;
-    }else if ( data[i].alarm_box_borough == "QUEENS" ) {
+    }else if ( data[i].alarm_box_borough == "QUEENS" && data[i].alarm_source_description_tx == alarm) {
       q++;
-    }else if ( data[i].alarm_box_borough == "MANHATTAN" ) {
+    }else if ( data[i].alarm_box_borough == "MANHATTAN" && data[i].alarm_source_description_tx == alarm) {
       m++;
-    }else {
+    }else if ( data[i].alarm_box_borough == "RICHMOND / STATEN ISLAND" && data[i].alarm_source_description_tx == alarm) {
       si++;
     }
   }
@@ -76,7 +74,7 @@ function FireIncident(){
     ["BROOKLYN",br],
     ["QUEENS",q],
     ["MANHATTAN",m],
-	  ["RICHMOND / STATEN ISLAND",si]
+    ["RICHMOND / STATEN ISLAND",si]
   ];
   let chartType = document.getElementById("chartType").value;
   displayChart(chartData,"chart", chartType);
