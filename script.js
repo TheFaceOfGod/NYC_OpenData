2da74691649dc3c234b8f2c88be335536bfeec0c
Elder Flame Doron <adrianl87@nycstudents.net>
2025-05-29 10:52:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,13 +29,15 @@ function showMap(location){
   let marker = L.marker(location).addTo(map);
   for(let i = 0; i < data.length; i++){
     let complaint = data[i];
-    build += `<div class="fitted card">`;
-    build += `     <h3>${complaint.incident_borough}</h3>`;    
-    build += `	   <hr>`;
-    build += `     <p>Location: ${complaint.incident_borough}</p>`;
-    build += `     <h4>${complaint.incident_borough}</h4>`;
-    build += `</div>`;
-    output.innerHTML=build;
+    if(complaint.incident_borough = address){
+      build += `<div class="fitted card">`;
+      build += `     <h3>${complaint.incident_borough}</h3>`;    
+      build += `	   <hr>`;
+      build += `     <p>Location: ${complaint.incident_borough}</p>`;
+      build += `     <h4>${complaint.incident_borough}</h4>`;
+      build += `</div>`;
+    }
+   output.innerHTML=build;
   }
 }     
 const geocodeWithNominatim = async (address) => {
