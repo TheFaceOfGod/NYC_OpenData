d4636934024c94e68cfe0b4c08f29c4c43de79c7
Elder Flame Doron <adrianl87@nycstudents.net>
2025-05-30 10:43:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -31,10 +31,9 @@ function showMap(location){
     let complaint = data[i];
     if(complaint.incident_borough = address){
       build += `<div class="fitted card">
-                  <h3>${complaint.incident_borough}</h3>    
-                  <hr>
-                  <p>Location: ${complaint.incident_borough}</p>
-                  <h4>${complaint.incident_borough}</h4>
+                  <h3>Borough:${complaint.incident_borough}</h3>    
+                  <p>Incident:${complaint.incident_classification}</p>
+                  <h4>Location of alarm:${complaint.alarm_box_location}</h4>
                 </div>`
     }
    output.innerHTML=build;
