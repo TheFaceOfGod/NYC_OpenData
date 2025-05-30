bde3744eae17d719f9ad257692933841d693c79f
Elder Flame Doron <adrianl87@nycstudents.net>
2025-05-30 10:28:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,12 +30,30 @@ function showMap(location){
   for(let i = 0; i < data.length; i++){
     let complaint = data[i];
     if(complaint.incident_borough = address){
-      build += `<div class="fitted card">`;
-      build += `     <h3>${complaint.incident_borough}</h3>`;    
-      build += `	   <hr>`;
-      build += `     <p>Location: ${complaint.incident_borough}</p>`;
-      build += `     <h4>${complaint.incident_borough}</h4>`;
-      build += `</div>`;
+          <div class="container">
+    </div>
+    build += `<div id="output" class="container" >
+                  <input style="font-size:1px;width:1px;text-align:right;" type="text" id="lat"> &nbsp; 
+                  <input style="font-size:1px;width:1px;text-align:right;" type="text" id="lon"> &nbsp;
+                  <select id="address" style="width:100px;flex:1000;text-align:center;">&nbsp;
+                    <option value="RICHMOND / STATEN ISLAND">RICHMOND / STATEN ISLAND</option>
+                    <option value="BROOKLYN">BROOKLYN</option>
+                    <option value="BRONX">BRONX</option>
+                    <option value="QUEENS">QUEENS</option>
+                    <option value="MANHATTAN">MANHATTAN</option>
+                  </select>
+                  <input type="button" onclick="displayLocation()" value="View">
+                </div>
+                <div style="display:flex; flex:1">
+                  <div style="display:flex; flex:1;" id="output"></div>
+                  <div style="display:flex; flex:1;" id="map"></div>
+                </div>
+                <div class="fitted card">
+                  <h3>${complaint.incident_borough}</h3>    
+                  <hr>
+                  <p>Location: ${complaint.incident_borough}</p>
+                  <h4>${complaint.incident_borough}</h4>
+                </div>`
     }
    output.innerHTML=build;
   }
