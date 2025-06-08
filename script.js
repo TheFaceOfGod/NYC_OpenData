b94f429f1ef02ccc8bc4612770bf67712d2d42e9
Elder Flame Doron <adrianl87@nycstudents.net>
2025-06-08 11:01:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function help(){
                 <p>Incident: ${c.incident_classification}</p>
                 <h4>Location of alarm: ${c.alarm_box_location}, ${c.incident_borough}</h4>
                 <h4>Zipcode: ${c.zipcode}</h4>
-              </div>`&nbsp;
+              </div>`;
   }
   output2.innerHTML=build2;
 }
@@ -50,7 +50,7 @@ function showMap(location){
                   <p>Incident: ${c.incident_classification}</p>
                   <h4>Location of alarm: ${c.alarm_box_location}</h4>
                   <h4>Zipcode: ${c.zipcode}</h4>
-                </div>`&nbsp;
+                </div>&nbsp`;
     }
   }
   output.innerHTML = build;
